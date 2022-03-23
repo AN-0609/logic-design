5e3098e66e348562051fc95fa1d268b4d970bc50
andy <970131andy@gmail.com>
2022-03-24 01:14:26 +0800

    add something

--- 0318/decoder_3_8.v
+++ 0318/decoder_3_8.v
@@ -1,46 +1,7 @@
 module decoder(E , In , Out);
-input E;
-input [2:0] In;
-output [7:0] Out;
-wire [7:0] Out;
-assign Out = E ? (8'b1 << In) : 8'h0;
+input E;                                //致能輸入
+input [2:0] In;                         //三位元輸入
+output [7:0] Out;                       //八位元輸出
+wire [7:0] Out;                         //排線
+assign Out = E ? (8'b1 << In) : 8'h0;   //若有致能輸入，將"00000001"依照輸入In之值左移。若無，則輸出"00000000"。
 endmodule
-// module decoder_3_8(E , In , Out);
-//     input E; 
-//     input [2:0] In;
-//     output [7:0] Out; 
-//     wire E1 , G1 , G2;
-//     not u1(E1 , In[2]); 
-//     and a1(G1 , E , In[2]);
-//     and a2(G2 , E , E1);
-//     decoder_2_4 block1(G1 , In[1 : 0] , Out[7 : 4]);
-//     decoder_2_4 block2(G2 , In[1 : 0] , Out[3 : 0]);
-// endmodule
-
-// module decoder(E , In , Out);
-// input E;
-// input [2:0] In;
-// output [7:0] Out;
-// wire [7:0] Out;
-// assign Out = E ? (8'b1 << In) : 8'h0;
-// endmodule
-
-// module decoder_2_4(E , In , Out);
-// input E; 
-// input [1:0] In;
-// output [3:0]Out; 
-// wire [3:0] Out;
-// assign Out = E ? 1'b1 << In : 4'h0;
-// endmodule
-
-// module clkgen(clka, clkb, clka_out, clkb_out);
-// input clka, clkb;
-// output clka_out, clkb_out;
-// reg clka_out, clkb_out;
-//     always @(clka) begin 
-//         clka_out = clka;
-//     end
-//     always @(clkb) begin
-//         clkb_out = clkb;
-//     end
-// endmodule
--- 0318/decoder_3_8_tb.v
+++ 0318/decoder_3_8_tb.v
@@ -1,57 +1,22 @@
 module decoder_3_8_tb;
-    reg E_tb;
-    reg [2:0] In_tb;
-    wire [7:0] Out_tb;
-    decoder decoder_1(.E(E_tb), .In(In_tb), .Out(Out_tb));
+    reg E_tb;           //智能輸入暫存
+    reg [2:0] In_tb;    //輸入暫存
+    wire [7:0] Out_tb;  //輸出暫存
+    decoder decoder_1(.E(E_tb), .In(In_tb), .Out(Out_tb));  //將輸入傳入decoder模組並取得輸出
 initial begin
-    #0 E_tb = 0; In_tb = 3'b000;
-    #10 E_tb = 1; In_tb = 3'b000;
-    #10 E_tb = 1; In_tb = 3'b001;
-    #10 E_tb = 1; In_tb = 3'b010;
-    #10 E_tb = 1; In_tb = 3'b011;
-    #10 E_tb = 1; In_tb = 3'b100;
-    #10 E_tb = 1; In_tb = 3'b101;
-    #10 E_tb = 1; In_tb = 3'b110;
-    #10 E_tb = 1; In_tb = 3'b111;
+    #0 E_tb = 0; In_tb = 3'b000;    //模擬輸入  致能關閉
+    #10 E_tb = 1; In_tb = 3'b000;   //模擬輸入  二進位數字000
+    #10 E_tb = 1; In_tb = 3'b001;   //模擬輸入  二進位數字001
+    #10 E_tb = 1; In_tb = 3'b010;   //模擬輸入  二進位數字010
+    #10 E_tb = 1; In_tb = 3'b011;   //模擬輸入  二進位數字011
+    #10 E_tb = 1; In_tb = 3'b100;   //模擬輸入  二進位數字100
+    #10 E_tb = 1; In_tb = 3'b101;   //模擬輸入  二進位數字101
+    #10 E_tb = 1; In_tb = 3'b110;   //模擬輸入  二進位數字110
+    #10 E_tb = 1; In_tb = 3'b111;   //模擬輸入  二進位數字111
     #10 $finish;
 end
 initial begin
-    $dumpfile("decoder.vcd");
-    $dumpvars(0, decoder_1);
+    $dumpfile("decoder_3_8.vcd");   //欲存入之檔案
+    $dumpvars(0, decoder_1);    //將所有變數存入
 end
 endmodule
-// module decoder_3_8_tb;
-//     reg E_tb;
-//     reg clka, clkb;
-//     reg [2:0] In_tb;
-//     wire [7:0] Out_tb;
-//     wire clka_out, clkb_out;
-//     decoder decoder_1(.E(E_tb), .In(In_tb), .Out(Out_tb));
-//     clkgen clkgen_1(.clka(clka), .clkb(clkb), .clka_out(clka_out), .clkb_out(clkb_out));
-//     initial begin
-//         clka = 1'b0; clkb = 1'b0;
-//     end
-//     always begin 
-//         #10 clka = ~clka; 
-//     end
-//     always begin
-//         #20 clkb = ~clkb;
-// end
-// initial begin
-//     #0 E_tb = 0; In_tb = 3'b000;
-//     #10 E_tb = 1; In_tb = 3'b000;
-//     #10 E_tb = 1; In_tb = 3'b001;
-//     #10 E_tb = 1; In_tb = 3'b010;
-//     #10 E_tb = 1; In_tb = 3'b011;
-//     #10 E_tb = 1; In_tb = 3'b100;
-//     #10 E_tb = 1; In_tb = 3'b101;
-//     #10 E_tb = 1; In_tb = 3'b110;
-//     #10 E_tb = 1; In_tb = 3'b111;
-//     #10 $finish;
-// end
-// initial begin
-//     $dumpfile("decoder.vcd");
-//     $dumpvars(0, decoder_1);
-//     $dumpvars(0, clkgen_1);
-// end
-// endmodule
